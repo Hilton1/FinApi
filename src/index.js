d256394042a67ce32556e8cfc1f658006edfa08a
Hilton1 <josehilton02@gmail.com>
2021-05-31 23:03:28 -0300

    Adding cpf validation

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-const costumers = [];
+const customers = [];
 
 app.use(express.json());
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
-  const id = uuidv4();
+  const customersAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+  
+  if (customersAlreadyExists) {
+    return response.status(400).json({error: 'Customer already exists!'})
+  }
 
-  costumers.push({
+  customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   });
 
