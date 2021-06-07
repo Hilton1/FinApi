f29d352cd75e42fb47144939892d8a6a337a7a36
Hilton1 <josehilton02@gmail.com>
2021-06-07 16:40:35 -0300

    adding business rule to search statement

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
-  const customersAlreadyExists = customers.some((customer) => customer.cpf === cpf);
-  
-  if (customersAlreadyExists) {
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
     return response.status(400).json({error: 'Customer already exists!'})
   }
 
@@ -27,10 +27,14 @@ app.post('/account', (request, response) => {
 
 });
 
-app.get('/statement/:cpf', (request, response) => {
-  const { cpf } = request.params;
+app.get('/statement', (request, response) => {
+  const { cpf } = request.headers;
 
   const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
 
   return response.json(customer.statement)
 });
